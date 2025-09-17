0d683267e825f0b7a7bd848eacbe60ba0b875df6
baongoc02 <tranthibaongoc779152@gmail.com>
2025-09-17 11:27:46 +0700

    update code

--- Websites/Restaurant/wwwroot/js/site.js
+++ Websites/Restaurant/wwwroot/js/site.js
@@ -1,7 +1,8 @@
-﻿const btn = document.getElementById('clickToTop');
+﻿//#region scroll
+const clickToTopBtn = document.getElementById('clickToTop');
 
 window.addEventListener('scroll', () => {
-    const navbar = document.querySelector('.headerContainer');
+    const navbar = document.querySelector('.headerDesktop');
 
     if (window.scrollY > 0) {
         navbar.classList.add('fixed');
@@ -10,12 +11,35 @@ window.addEventListener('scroll', () => {
     }
 
     if (window.scrollY > 300) {
-        btn.classList.add('show');
+        clickToTopBtn.classList.add('show');
     } else {
-        btn.classList.remove('show');
+        clickToTopBtn.classList.remove('show');
     }
 });
 
-btn.addEventListener('click', () => {
+clickToTopBtn.addEventListener('click', () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
 });
+//#endregion
+//#region header mobile
+const mobileNav = document.getElementById("mobileNav");
+const toggleHeaderBtn = document.getElementById("toggleHeader");
+const closeNavBtn = document.getElementById("closeNav");
+const overlayBackground = document.getElementById('overlayBackground');
+function closeNav() {
+    mobileNav.classList.remove("navOpen");
+    overlayBackground.style.display = 'none'
+    document.body.style.overflow = 'auto';
+}
+toggleHeaderBtn.addEventListener('click', () => {
+    document.body.style.overflow = 'hidden';
+    mobileNav.classList.add("navOpen");
+    overlayBackground.style.display = 'block';
+});
+closeNavBtn.addEventListener('click', () => {
+    closeNav();
+});
+overlayBackground.addEventListener('click', () => {
+    closeNav();
+});
+//#endregion
